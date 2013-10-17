c3b800abaf4a690326e93c3f002c5aad12f28fba
vincentmarin <bombophone@gmail.com>
2013-10-17 22:52:49 +0200

    Contenu0 de Assets
    point de départ

---
+++ Scripts/AvanceePretre.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+function Start () {
+}
+
+function Update () {
+//	if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+//	{ 
+		if (transform.position.z > 4) 
+	   {
+		   transform.Translate(0, 0, 4*Time.deltaTime);
+		   animation.CrossFade("Run");
+	   }
+	   if (transform.position.z <= 4 && transform.position.z >0) 
+	   {
+		   transform.Translate(0, 0, 4*Time.deltaTime);
+		   animation.CrossFade("Walk");
+	   }
+	   if (transform.position.z <= 0) 
+	   {
+	   animation.CrossFade("Speak");
+	   }
+ //  }
+}
---
+++ Scripts/OuverturePorteDroite.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+var OrientationInitialeDroite = 0;
+OrientationInitialeDroite = transform.eulerAngles.y;
+function Update () {
+	//if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+		//{
+		if (Mathf.Abs(transform.eulerAngles.y-OrientationInitialeDroite) < 90) 
+	   {
+		transform.RotateAround (Vector3(315,0,495.514), Vector3(0,1,0), 40 * Time.deltaTime);
+		}
+	//}
+}
